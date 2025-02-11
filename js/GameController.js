3b8cc13023635a6ee8268ace37968db52d9db7b7
Dan H <daniel.c.hodgson@address.com>
2025-02-11 13:08:33 +0000

    Added basic start menu with name entering

--- js/GameController.js
+++ js/GameController.js
@@ -4,95 +4,97 @@ import Player from "./Player.js";
 import GameState from "./GameState.js";
 
 const GameController = (function () {
-    let gameBoard = new GameBoard(createInitialBoard());
-    const playerOne = new Player("playerOne", "O");
-    const playerTwo = new Player("playerTwo", "X");
-    const players = [playerOne, playerTwo];
-    let gameStates = [new GameState(1, gameBoard, playerOne, "ongoing")];
+    return function (playerOneName, playerTwoName) {
+        let gameBoard = new GameBoard(createInitialBoard());
+        const playerOne = new Player(playerOneName, "O");
+        const playerTwo = new Player(playerTwoName, "X");
+        const players = [playerOne, playerTwo];
+        let gameStates = [new GameState(1, gameBoard, playerOne, "ongoing")];
 
-    function getCurrentGameState() {
-        return gameStates[gameStates.length - 1];
-    }
-
-    function createInitialBoard() {
-        return Array(3).fill(null).map(() => Array(3).fill(null).map(() => new Cell(null)));
-    }
-
-    function cloneBoardGrid(grid) {
-        return grid.map(row => row.map(cell => new Cell(cell.getValue())));
-    }
-
-    const switchActivePlayer = (activePlayer) => {
-        let newPlayer = activePlayer.getMarker() === players[0].getMarker() ? players[1] : players[0];
-        return newPlayer;
-    };
-
-
-    function playRound(row, col) {
-        const currentState = getCurrentGameState();
-        const currentBoard = currentState.getGameBoard();
-        const cell = currentBoard.getCell(row, col);
-
-        if (cell.isOccupied()) {
-            console.warn("Square occupied, choose another!");
-            return;
+        function getCurrentGameState() {
+            return gameStates[gameStates.length - 1];
         }
 
-        const turn = currentState.getTurn() + 1;
-        const activePlayer = currentState.getActivePlayer();
-        const newBoardGrid = cloneBoardGrid(currentBoard.getBoard());
-        const newCurrentBoard = new GameBoard(newBoardGrid);
-        newCurrentBoard.addMarker(row, col, activePlayer.marker);
-
-        const gameWon = isGameWon(newBoardGrid, activePlayer);
-
-        if (gameWon) {
-            gameStates.push(new GameState(turn, newCurrentBoard, activePlayer, "won"));
-        } else if (!newCurrentBoard.boardHasSpace()) {
-            gameStates.push(new GameState(turn, newCurrentBoard, activePlayer, "draw"));
-        } else {
-            const newActivePlayer = switchActivePlayer(activePlayer);
-            gameStates.push(new GameState(turn, newCurrentBoard, newActivePlayer, "ongoing"));
+        function createInitialBoard() {
+            return Array(3).fill(null).map(() => Array(3).fill(null).map(() => new Cell(null)));
         }
-    }
 
-    function isGameWon(board, player) {
-        const size = board.length;
-        const marker = player.marker;
+        function cloneBoardGrid(grid) {
+            return grid.map(row => row.map(cell => new Cell(cell.getValue())));
+        }
 
-        // Check rows and columns
-        for (let i = 0; i < size; i++) {
-            if (board[i].every(cell => cell.getValue() === marker) ||
-                board.map(row => row[i]).every(cell => cell.getValue() === marker)) {
-                return true;
+        const switchActivePlayer = (activePlayer) => {
+            let newPlayer = activePlayer.getMarker() === players[0].getMarker() ? players[1] : players[0];
+            return newPlayer;
+        };
+
+
+        function playRound(row, col) {
+            const currentState = getCurrentGameState();
+            const currentBoard = currentState.getGameBoard();
+            const cell = currentBoard.getCell(row, col);
+
+            if (cell.isOccupied()) {
+                console.warn("Square occupied, choose another!");
+                return;
+            }
+
+            const turn = currentState.getTurn() + 1;
+            const activePlayer = currentState.getActivePlayer();
+            const newBoardGrid = cloneBoardGrid(currentBoard.getBoard());
+            const newCurrentBoard = new GameBoard(newBoardGrid);
+            newCurrentBoard.addMarker(row, col, activePlayer.marker);
+
+            const gameWon = isGameWon(newBoardGrid, activePlayer);
+
+            if (gameWon) {
+                gameStates.push(new GameState(turn, newCurrentBoard, activePlayer, "won"));
+            } else if (!newCurrentBoard.boardHasSpace()) {
+                gameStates.push(new GameState(turn, newCurrentBoard, activePlayer, "draw"));
+            } else {
+                const newActivePlayer = switchActivePlayer(activePlayer);
+                gameStates.push(new GameState(turn, newCurrentBoard, newActivePlayer, "ongoing"));
             }
         }
 
-        // Check diagonals
-        if (board.every((_, i) => board[i][i].getValue() === marker) ||
-            board.every((_, i) => board[i][size - 1 - i].getValue() === marker)) {
-            return true;
+        function isGameWon(board, player) {
+            const size = board.length;
+            const marker = player.marker;
+
+            // Check rows and columns
+            for (let i = 0; i < size; i++) {
+                if (board[i].every(cell => cell.getValue() === marker) ||
+                    board.map(row => row[i]).every(cell => cell.getValue() === marker)) {
+                    return true;
+                }
+            }
+
+            // Check diagonals
+            if (board.every((_, i) => board[i][i].getValue() === marker) ||
+                board.every((_, i) => board[i][size - 1 - i].getValue() === marker)) {
+                return true;
+            }
+
+            return false;
         }
 
-        return false;
-    }
-
-    function reset() {
-        gameBoard = new GameBoard(createInitialBoard());
-        gameStates = [new GameState(1, gameBoard, playerOne, "ongoing")];
-    }
-
-    function undoRound() {
-        if (gameStates.length > 1) {
-            gameStates.pop();
+        function reset() {
+            gameBoard = new GameBoard(createInitialBoard());
+            gameStates = [new GameState(1, gameBoard, playerOne, "ongoing")];
         }
-    }
 
-    return {
-        getCurrentGameState,
-        playRound,
-        undoRound,
-        reset,
+        function undoRound() {
+            if (gameStates.length > 1) {
+                gameStates.pop();
+            }
+        }
+
+        return {
+            getCurrentGameState,
+            playRound,
+            undoRound,
+            reset,
+        }
     }
 })();
 
--- js/app.js
+++ js/app.js
@@ -1,5 +1,49 @@
 import GameController from "./GameController.js";
 import ScreenController from "./ScreenController.js";
 
-const gameController = GameController;
-const screenController = ScreenController(gameController);
+const App = (function () {
+
+    const startModal = document.querySelector(".input");
+    const playerOneField = startModal.querySelector("#player-one-name");
+    const playerTwoField = startModal.querySelector("#player-two-name");
+    const playBtn = startModal.querySelector("#play-btn");
+    const gameContainer = document.querySelector(".game-container");
+    let gameController = null;
+
+
+    function handlePlayClick(event) {
+        event.preventDefault();
+
+        let playerOneName = playerOneField.value === "" ? "playerOne" : playerOneField.value;
+        let playerTwoName = playerOneField.value === "" ? "playerTwo" : playerTwoField.value;
+        initGame(playerOneName, playerTwoName);
+        startModal.remove();
+        unBindEvents();
+        gameContainer.style.display = "grid";
+    }
+    
+    function initGame(playerOneName, playerTwoName) {
+        gameController = GameController(playerOneName, playerTwoName);
+        ScreenController(gameController);
+    }
+
+    function unBindEvents() {
+        playBtn.removeEventListener("click", handlePlayClick);
+    }
+
+    function bindEvents() {
+        playBtn.addEventListener("click", handlePlayClick);
+    }
+
+
+    bindEvents();
+
+    return {
+    }
+
+})();
+
+
+
+
+const app = App;
